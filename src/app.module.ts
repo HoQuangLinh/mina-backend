dc7f4e77468d692705db790bc936257fd4e32ddf
HoQuangLinh <71378783+HoQuangLinh@users.noreply.github.com>
2024-10-01 23:07:11 +0700

    Add mongoose and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const mongoUri = configService.get<string>('MONGO_URI');
+        return {
+          uri: mongoUri,
+          user: configService.get<string>('MONGO_USER'),
+          pass: configService.get<string>('MONGO_PASSWORD'),
+          dbName: configService.get<string>('MONGO_DB'),
+        };
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
