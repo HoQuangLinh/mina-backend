9a05c19d7bc8490d4f1aa23dc80004cd2b70fab1
HoQuangLinh <71378783+HoQuangLinh@users.noreply.github.com>
2024-10-15 16:56:37 +0700

    Add local strategy login feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const mongoUri = configService.get<string>('MONGO_URI');
-        return {
-          uri: mongoUri,
-        };
-      },
-    }),
+
     DatabaseModule,
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
