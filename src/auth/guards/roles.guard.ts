9b9da796446214e02520fd9a276858cc588e44ce
Ho Quang Linh <hoquanglinh2710@gmail.com>
2024-11-14 16:51:40 +0700

    Add roles guard for authoration app

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,32 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from 'src/decorators/roles.decorator';
+import { UserRole } from 'src/users/enums/user.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    console.log({ requiredRoles, role: user.role });
+    const hasRole = requiredRoles.includes(user.role);
+    if (!hasRole) {
+      throw new ForbiddenException(
+        `You don't have permission to access resource`,
+      );
+    }
+    return true;
+  }
+}
